f8d0b054a9ec765411b275d60a7c2ffe1175954a
Matheus Acfer <matheusacfer@EU3146037018865.local>
2025-04-14 15:30:22 -0300

    refactor: update create users service to automatic create register

--- src/entities/users/repositories/implementations/prisma-users-repository.ts
+++ src/entities/users/repositories/implementations/prisma-users-repository.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/singletons/prisma';
 import type { UsersRepository } from '../users-repository';
+import { AppError } from '@/error/AppError';
 
 export class PrismaUsersRepository implements UsersRepository {
   async findById(id: string) {
@@ -38,12 +39,41 @@ export class PrismaUsersRepository implements UsersRepository {
   }
 
   async create({ lotteryEmployement_id, ...userData }: CreateUserDTO) {
+    const lotteryEmployement = await prisma.lotteryEmployees.findUnique({
+      where: {
+        id: lotteryEmployement_id,
+      },
+      select: {
+        id: true,
+        lottery: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+
+    if (!lotteryEmployement)
+      throw new AppError(
+        'Lottery Employement not found, missing validation in service',
+        500,
+      );
+
     const user = await prisma.user.create({
       data: {
         ...userData,
         lotteryEmployement: {
           connect: {
-            id: lotteryEmployement_id,
+            id: lotteryEmployement.id,
+          },
+        },
+        register: {
+          create: {
+            lottery: {
+              connect: {
+                id: lotteryEmployement.lottery.id,
+              },
+            },
           },
         },
       },
